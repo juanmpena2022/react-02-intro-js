925e2dc1db618e17e75100f0f51ef21eb0108452
Juan Miguel <juanmpena2022@gmail.com>
2022-03-08 09:41:34 -0300

    Adicionar archivos index en 07-heroes-app

---
+++ src/07-heroes-app/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HeroesApp } from "./07-heroes-app/HeroesApp";
+
+import "./index.css";
+
+const divRoot = document.getElementById("root");
+ReactDOM.render(<HeroesApp />, divRoot);
